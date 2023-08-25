6a3992f708528ac122f26a87dd8c10c6a813bb93
blakedaniel <blake.vanfleteren@gmail.com>
2023-08-25 16:48:01 -0400

    switched to xml

--- brand_scanner/main.py
+++ brand_scanner/main.py
@@ -5,6 +5,16 @@ import openai
 from llama_index import SimpleDirectoryReader
 from llama_hub.web.sitemap.base import SitemapReader
 import os
+from urllib.request import Request, urlopen
+
+class SitemapeReaderAgent(SitemapReader):
+    def _load_sitemap(self, sitemap_url: str) -> str:
+        user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"
+        headers = {'User-Agent': user_agent}
+        req = Request(sitemap_url, headers=headers)
+        sitemap_url_request = urlopen(req)
+        return sitemap_url_request.read()
+
 
 # set openai api key and header
 openai.api_key = st.secrets.openai_key
@@ -20,7 +30,7 @@ if 'messages' not in st.session_state.keys():
 @st.cache_resource(show_spinner=False)
 def load_data():
     with st.spinner(text="Loading and indexing the GMMB pages – hang tight! This should take 1-2 minutes."):
-        reader = SitemapReader()
+        reader = SitemapeReaderAgent()
         docs = reader.load_data(sitemap_url='https://www.gmmb.com/sitemap_index.xml')
         service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are a friendly sales expert on GMMB services and offering and your job is to answer content and sales questions. Assume that all questions are related to GMMB services, work, and culture. Keep your answers in the tone of GMMB and based on facts – do not hallucinate features."))
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
