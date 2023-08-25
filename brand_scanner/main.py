6dbfd8116a2de96d54228cc7a409ae07ac2edc89
blakedaniel <blake.vanfleteren@gmail.com>
2023-08-25 14:52:59 -0400

    added site files

---
+++ brand_scanner/main.py
@@ -0,0 +1,46 @@
+import streamlit as st
+from llama_index import VectorStoreIndex, ServiceContext, Document
+from llama_index.llms import OpenAI
+import openai
+from llama_index import SimpleDirectoryReader
+from website_downloader import save_pages
+
+# set openai api key and header
+openai.api_key = st.secrets.openai_key
+st.header('Chat with an Online Brand')
+
+# initialize messages with starting message if new conversation
+if 'messages' not in st.session_state.keys():
+    st.session_state.messages = [
+        {'role': 'brand strategist',
+         'content': 'ask me a brand related question about Mighty Good!'}
+    ]
+
+@st.cache_resource(show_spinner=False)
+def load_data():
+    with st.spinner(text="Loading and indexing the Mighty Good pages – hang tight! This should take 1-2 minutes."):
+        reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
+        docs = reader.load_data()
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are a sales expert on Mighty Good services and offering and your job is to answer content and sales questions. Assume that all questions are related to Might Good services and content. Keep your answers technical and based on facts – do not hallucinate features."))
+        index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+        return index
+
+index = load_data()
+
+chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+
+if prompt := st.chat_input("Your GMMB question"): # Prompt for user input and save to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages: # Display the prior chat messages
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+# If last message is not from assistant, generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = chat_engine.chat(prompt)
+            st.write(response.response)
+            message = {"role": "assistant", "content": response.response}
+            st.session_state.messages.append(message) # Add response to message history
---
+++ brand_scanner/website_downloader.py
@@ -0,0 +1,64 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+import re
+
+def get_page_urls(url, max_pages:(int | None)):
+    base_url = url
+    stack = [base_url]
+    urls = set()
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"
+    
+    while stack:
+        url = stack.pop()
+        response = requests.get(url, headers={'User-Agent': user_agent})
+        soup = BeautifulSoup(response.content, 'html.parser')
+        for link in soup.find_all('a', href=True):
+            url = link['href']
+            if not url.startswith('http') or base_url not in url or url in urls:
+                continue
+            urls.add(url)
+            stack.append(url)
+            if max_pages and len(urls) == max_pages:
+                return urls
+    return urls
+
+def create_file_path(url:str) -> None:
+    path_comps = url.split('/')
+    if path_comps[-1] == '':
+        path_comps.pop()
+    directory = os.path.dirname(os.getcwd())
+    directory = os.path.join(directory, 'data')
+    
+    for comp in path_comps[:-1]:
+        directory = os.path.join(directory, comp)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    file_name = path_comps[-1] + '.md'
+    file_path = os.path.join(directory, file_name)
+    return file_path
+
+def save_pages(homepage_url:str, max_pages:int=None):
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"
+    urls = get_page_urls(homepage_url, max_pages)
+    for url in urls:
+        response = requests.get(url, headers={'User-Agent': user_agent})
+        if response.status_code != 200:
+            continue
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        subs = {'https://www.': '', '.': '_'}
+        for orig, sub in subs.items():
+            url = url.replace(orig, sub)
+            
+        file_path = create_file_path(url)
+                
+        with open(file_path, 'w') as file:
+            if not soup.main:
+                continue
+            text = soup.main.text
+            text = re.sub(r'(\n){2,}|(\t){2,}', r'\n', text)
+            markdown = f'# {soup.title.string}\n\n{text}'
+            file.write(markdown)
+
+save_pages('https://www.gmmb.com', 50)
